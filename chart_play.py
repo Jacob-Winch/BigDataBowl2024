f89093b09e24fe5d9652afd410b8ef2c67b0b75c
Colton Schneider <colton@pambra.ca>
2024-01-04 17:02:36 -0700

    Updated chart play function to be more dynamic

--- chart_play.py
+++ chart_play.py
@@ -5,6 +5,7 @@ import matplotlib.patches as patches
 from matplotlib.markers import MarkerStyle
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import math
 import psycopg2
 from sys import argv
 import os
@@ -71,8 +72,8 @@ def visualize_frame(game_id: int, play_id: int, home_color: str = 'cornflowerblu
     cur.execute("SELECT defensive_team FROM plays WHERE game_id=%s AND play_id=%s", (game_id, play_id))
     defensive_team = cur.fetchone()[0]
 
-    cur.execute("SELECT player_id, team, orientation, x, y, speed_x, speed_y, acc_x, acc_y FROM tracking "
-                "WHERE game_id=%s AND play_id=%s AND event='handoff'", (game_id, play_id))
+    cur.execute("SELECT player_id, team, orientation, x, y, speed, acceleration FROM tracking "
+                "WHERE game_id=%s AND play_id=%s AND event='pass_arrived'", (game_id, play_id))
     step_info = cur.fetchall()
 
     # # predict tackle probability
@@ -87,6 +88,18 @@ def visualize_frame(game_id: int, play_id: int, home_color: str = 'cornflowerblu
     #     else:
     #         data.append([row[3] - ball_carrier[0], abs(ball_carrier[1] - row[4])])
     # probabilities = model.predict_proba(data)
+    # predict tackle probability
+    cur.execute("SELECT x, y, lr FROM tracking t, plays p "
+                "WHERE t.game_id=%s AND p.game_id=%s AND t.play_id=%s AND p.play_id=%s AND t.player_id=p.ball_carrier "
+                "AND event='pass_arrived'",
+                (game_id, game_id, play_id, play_id))
+    ball_carrier = cur.fetchone()
+    data = []
+    for row in step_info:
+        data.append([math.sqrt(abs(ball_carrier[1] - row[4])),
+                     math.sqrt(math.sqrt((ball_carrier[0] - row[3]) ** 2 + (ball_carrier[1] - row[4]) ** 2)),
+                     row[5], row[6]])
+    probabilities = model.predict_proba(data)
 
     """orientation = []
     for player in step_info:
@@ -117,10 +130,10 @@ def visualize_frame(game_id: int, play_id: int, home_color: str = 'cornflowerblu
         marker1._transform.rotate_deg(360 - row[2])
         if row[1] == defensive_team:
             ax.scatter(row[3], row[4], marker=marker1, s=150, color=color,
-                       label="{0} - {1}".format(row[0] % 100, probabilities[i][1]))
+                       label="{0} - {1}".format(row[0] % 100, probabilities[i][1]), zorder=2)
             ax.text(row[3], row[4], str(row[0] % 100))
         else:
-            ax.scatter(row[3], row[4], marker=marker1, s=150, color=color)
+            ax.scatter(row[3], row[4], marker=marker1, s=150, color=color, zorder=2)
 
     # set axis title
     ax.set_title(f'Tracking data for {game_id} {play_id}')
@@ -131,6 +144,7 @@ def visualize_frame(game_id: int, play_id: int, home_color: str = 'cornflowerblu
 
 def visualize_play(game_id: int, play_id: int, title_str: str):
     """Visualize a play from the dataset"""
+    model = load("distance.joblib")
     conn = psycopg2.connect(connection_string)
     cur = conn.cursor()
     cur.execute("SELECT MAX(frame_id) FROM tracking WHERE game_id=%s AND play_id=%s", (game_id, play_id))
@@ -139,10 +153,10 @@ def visualize_play(game_id: int, play_id: int, title_str: str):
     og_frames = og_frames // frame_mod
     interval_ms = 100 * frame_mod
     fig, ax = plt.subplots(figsize=(12, 5.33))
-    
-    cur.execute("SELECT frame_id FROM tracking WHERE game_id=%s AND play_id=%s AND event LIKE 'pass_arrived'", (game_id, play_id)) 
+
+    cur.execute("SELECT frame_id FROM tracking WHERE game_id=%s AND play_id=%s AND event LIKE 'pass_arrived'", (game_id, play_id))
     pass_arrived_frame = cur.fetchone()[0] if cur.rowcount != 0 else None
-    
+
     pause_duration_frames = 10
 
     # Increase the total number of frames to include the pause
@@ -151,24 +165,37 @@ def visualize_play(game_id: int, play_id: int, title_str: str):
     cur.execute("SELECT MAX(frame_id) FROM tracking WHERE game_id=%s AND play_id=%s", (game_id, play_id))
     max_step = cur.fetchone()[0]
     step_list = np.linspace(1, max_step, frames)
-    
+
+    cur.execute("SELECT player_id, team, orientation, x, y, speed, acceleration, p.name FROM tracking, players p "
+                "WHERE player_id=%s AND game_id=%s AND play_id=%s AND event='pass_arrived' AND player_id=p.id",
+                (argv[4], game_id, play_id))
+    tackler = cur.fetchone()
+    cur.execute("SELECT x, y, lr FROM tracking t, plays p "
+                "WHERE t.game_id=%s AND p.game_id=%s AND t.play_id=%s AND p.play_id=%s AND t.player_id=p.ball_carrier "
+                "AND event='pass_arrived'",
+                (game_id, game_id, play_id, play_id))
+    ball_carrier = cur.fetchone()
+    data = [[math.sqrt(abs(ball_carrier[1] - tackler[4])),
+             math.sqrt(math.sqrt((ball_carrier[0] - tackler[3]) ** 2 + (ball_carrier[1] - tackler[4]) ** 2)),
+             tackler[5], tackler[6]]]
+    probability = model.predict_proba(data)[0]
 
     def animate(i: int, game_id: int, play_id: int, frames: int, home_color: str = 'cornflowerblue',
-                away_color: str = 'coral'):
+                away_color: str = 'coral', home_text_color: str = 'white', away_text_color: str = 'white'):
         ax.clear()
         create_football_field(fig, ax)
-        
-        home_patch = Patch(color=home_color, label=f'Home Team: New Orleans Saints')  
-        away_patch = Patch(color=away_color, label=f'Away Team: Cincinnati Bengals')  
-    
-        legend = ax.legend(handles=[home_patch, away_patch], 
+
+        home_patch = Patch(color=home_color, label=f'Home Team: New Orleans Saints')
+        away_patch = Patch(color=away_color, label=f'Away Team: Cincinnati Bengals')
+
+        legend = ax.legend(handles=[home_patch, away_patch],
                        loc='upper left', frameon=True, handlelength=0, handletextpad=0)
         legend.get_frame().set_facecolor('lightgray')  # Set the legend background color
         legend.get_frame().set_edgecolor('black')  # Optionally, set the legend border color
         legend.get_frame().set_alpha(0.8)  # Optionally, set the transparency of the background
         """Function to animate player tracking data"""
         if pass_arrived_frame and pass_arrived_frame <= i <= pass_arrived_frame + pause_duration_frames:
-            
+
             cur.execute("SELECT player_id, team, orientation, x, y, speed_x, speed_y, acc_x, acc_y, jerseynumber FROM tracking "
                         "WHERE game_id=%s AND play_id=%s AND frame_id=%s", (game_id, play_id, pass_arrived_frame))
             paused_step_info = cur.fetchall()
@@ -182,19 +209,21 @@ def visualize_play(game_id: int, play_id: int, title_str: str):
                     home_team = row[1]
                 if row[1] == home_team:
                     color = home_color
+                    text_color = home_text_color
                 else:
                     color = away_color
+                    text_color = away_text_color
                 marker1 = MarkerStyle(r'o')
                 marker1._transform.rotate_deg(360 - row[2])
                 ax.scatter(row[3], row[4], marker=marker1, s=150, color=color, zorder = 2)
                 player_number = str(int(row[9]))
-                ax.text(row[3], row[4], player_number, color = 'white', fontsize = 8, ha = 'center', va = 'center')
-                
-                if row[0] == 43299:
+                ax.text(row[3], row[4], player_number, color = text_color, fontsize = 8, ha = 'center', va = 'center')
+
+                if row[0] == int(argv[4]):
                     circle = plt.Circle((row[3], row[4]), 1.5, color='yellow', fill=False, lw=2)
                     ax.add_patch(circle)
-                    textstr = f"Eli Apple: Tackle Probability: 76.7%"
-                    ax.annotate(textstr, 
+                    textstr = "{0}: Tackle Probability: {1.2f}%".format(tackler[-1], probability[1] * 100)
+                    ax.annotate(textstr,
                             xy=(row[3], row[4]), xycoords='data',
                             xytext=(0.75, 0.83), textcoords='axes fraction',
                             arrowprops=dict(arrowstyle="->", connectionstyle="arc3"),
@@ -202,13 +231,13 @@ def visualize_play(game_id: int, play_id: int, title_str: str):
                             fontsize=12)
             ax.set_title(f"{title_str}", fontsize=14, fontweight='bold')
             return
-                    
+
         if i > (pass_arrived_frame + pause_duration_frames):
             i -= pause_duration_frames
-        
+
         # create fresh field
         step = int(step_list[i])
-        
+
         # subset data to step info
         cur.execute("SELECT player_id, team, orientation, x, y, speed_x, speed_y, acc_x, acc_y, jerseynumber FROM tracking "
                     "WHERE game_id=%s AND play_id=%s AND frame_id=%s", (game_id, play_id, step))
@@ -225,25 +254,26 @@ def visualize_play(game_id: int, play_id: int, title_str: str):
                 home_team = row[1]
             if row[1] == home_team:
                 color = home_color
+                text_color = home_text_color
             else:
                 color = away_color
-            marker1 = MarkerStyle(r'o')
+                text_color = away_text_color
+            marker1 = MarkerStyle(r'$\spadesuit$')
             marker1._transform.rotate_deg(360 - row[2])
-            ax.scatter(row[3], row[4], marker=marker1, s=150, color=color, zorder = 2)
-            player_number = str(int(row[9]))
-            ax.text(row[3], row[4], player_number, color = 'white', fontsize = 8, ha = 'center', va = 'center')
-        ax.set_title(f"{title_str}", fontsize=14, fontweight='bold')
+            ax.scatter(row[3], row[4], marker=marker1, s=150, color=color)
 
-    anim = animation.FuncAnimation(fig, animate, fargs=(game_id, play_id, frames), frames=frames + pause_duration_frames, repeat=False,
+        # set axis title
+        ax.set_title(f'Tracking data for {game_id} {play_id} at step {step}')
+
+    anim = animation.FuncAnimation(fig, animate, fargs=(game_id, play_id, frames), frames=frames, repeat=False,
                                    interval=interval_ms)
-    plt.show()
-    anim.save("play.gif", writer="imagemagick", fps=10)
+    anim.save("{0}.gif".format(argv[5]), writer="imagemagick", fps=10)
     conn.close()
 
 
 def visualize_speed(game_id: int, play_id: int, home_color: str = "cornflowerblue", away_color: str = "coral"):
     """Visualize speed vectors for a play"""
-    conn = psycopg2.connect(connection_string)
+    conn = psycopg2.connect("dbname=BigDataBowl user=cschneider")
     cur = conn.cursor()
     fig, ax = plt.subplots(figsize=(12, 5.33))
     # create fresh field
@@ -267,7 +297,7 @@ def visualize_speed(game_id: int, play_id: int, home_color: str = "cornflowerblu
             color = home_color
         else:
             color = away_color
-        marker1 = MarkerStyle('o')
+        marker1 = MarkerStyle(r'$\spadesuit$')
         marker1._transform.rotate_deg(360 - row[2])
         ax.scatter(row[3], row[4], marker=marker1, s=150, color=color)
         ax.arrow(row[3], row[4], row[5], row[6], color="firebrick", width=0.25)
@@ -279,8 +309,12 @@ def visualize_speed(game_id: int, play_id: int, home_color: str = "cornflowerblu
 
 if __name__ == "__main__":
     if argv[1] == "-p":
-        visualize_play(int(argv[2]), int(argv[3]), "T.Hill pass short left to M.Callaway to CIN 30 for 9 yards")
-        #visualize_frame(int(argv[2]), int(argv[3]))
+        conn = psycopg2.connect(connection_string)
+        cur = conn.cursor()
+        cur.execute("SELECT play FROM plays WHERE game_id=%s AND play_id=%s", (argv[2], argv[3]))
+        play_description = cur.fetchone()[0]
+        play_description = play_description.split(")")[2][1:]
+        play_description = play_description.split("(")[0][:-1]
+        visualize_play(int(argv[2]), int(argv[3]), play_description)
     elif argv[1] == "-v":
         visualize_speed(int(argv[2]), int(argv[3]))
-
